435aed3585132deb9193ca4a4927ba16364827a3
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-01-13 23:17:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Задача 4.1..cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число N, при условие N > 0" + Environment.NewLine);
+            int N = Convert.ToInt32(Console.ReadLine());
+            int n = 0;
+            int i = 1;
+            for (; i <= N; i++)
+            {
+                n += 2 * i - 1;
+                Console.WriteLine("Квадрат числа {0} равен {1}", N, n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
